e25c25a931f9d45844809a4ca5a54a17431048cd
siseldyke <siseldyke@gmail.com>
2024-10-28 10:14:57 -0400

    exercise 6 finished?

--- exercises.js
+++ exercises.js
@@ -67,4 +67,24 @@ const car = {
   console.log(myCar.model)
 
   // Your code here
-  
+
+
+  //exercise 6
+  // Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+const userProfile = {
+    username: 'stef',
+    name: 'stefan',
+    age: 36
+}
+propertyName = {
+    [userProfile.username]:'siseldyke'
+}
+
+console.log(userProfile)
+console.log(propertyName)
+
